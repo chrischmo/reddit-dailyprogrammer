104bc6757d056beed89dd315731bde07b63c43d0
Christian Schmitt <chrischmo@googlemail.com>
2012-02-10 17:31:23 +0100

    Replaced string concatenation with string formatting

--- 120210_difficult.py
+++ 120210_difficult.py
@@ -1,10 +1,10 @@
 def guess_num(range, guesses = 1):
     """Tries to guess a number in a range given as an array by narrowing down the range recursively"""
-    candidate = (range[0] + range[1]) / 2  #medium value of current range
-    answer = raw_input("Is it " + str(candidate) + "? Answer 'h' for 'higher', 'l' for 'lower', 'r' for 'right': ")
+    candidate = (range[0] + range[1]) / 2   #median of current range
+    answer = raw_input("Is it {0}? Answer 'h' for 'higher', 'l' for 'lower', 'r' for 'right': ".format(candidate))
     if answer == "h": guess_num([candidate + 1, range[1]], guesses + 1)
     elif answer == "l": guess_num([range[0], candidate - 1], guesses + 1)
-    elif answer == "r": print "HA! And it took me only " + str(guesses) + " guesses!"
+    elif answer == "r": print "HAH! And it took me only {0} guesses!".format(guesses)
     else: guess_num(range, guesses)   #catches invalid answers
 
 num = raw_input("Please enter a whole number between 1 and 100 and I'll try to guess it: ")
