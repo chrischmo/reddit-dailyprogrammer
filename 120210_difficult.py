e0f7668a8ce61ade86e4d435958c532b9b65b213
Christian Schmitt <chrischmo@googlemail.com>
2012-02-10 17:34:45 +0100

    Gave clearer variable name to the guess counter

--- 120210_difficult.py
+++ 120210_difficult.py
@@ -1,11 +1,11 @@
-def guess_num(range, guesses = 1):
+def guess_num(range, counter = 1):
     """Tries to guess a number in a range given as an array by narrowing down the range recursively"""
     candidate = (range[0] + range[1]) / 2   #median of current range
     answer = raw_input("Is it {0}? Answer 'h' for 'higher', 'l' for 'lower', 'r' for 'right': ".format(candidate))
-    if answer == "h": guess_num([candidate + 1, range[1]], guesses + 1)
-    elif answer == "l": guess_num([range[0], candidate - 1], guesses + 1)
-    elif answer == "r": print "HAH! And it took me only {0} guesses!".format(guesses)
-    else: guess_num(range, guesses)   #catches invalid answers
+    if answer == "h": guess_num([candidate + 1, range[1]], counter + 1)
+    elif answer == "l": guess_num([range[0], candidate - 1], counter + 1)
+    elif answer == "r": print "HAH! And it took me only {0} guesses!".format(counter)
+    else: guess_num(range, counter)   #catches invalid answers
 
 num = raw_input("Please enter a whole number between 1 and 100 and I'll try to guess it: ")
 guess_num([1, 100])
