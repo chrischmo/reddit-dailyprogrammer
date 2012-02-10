ae92ba7bbc39c1e502de91c29cf060c0c6dff460
Christian Schmitt <chrischmo@googlemail.com>
2012-02-10 22:21:42 +0100

    Added readme

--- 120210_difficult.py
+++ 120210_difficult.py
@@ -1,6 +1,7 @@
 def guess_num(range, counter = 1):
     """Tries to guess a number in a range given as an array by narrowing down the range recursively"""
     candidate = (range[0] + range[1]) / 2   #gets median of current range
+    print range
     answer = raw_input("Is it {0}? Answer 'h' for 'higher', 'l' for 'lower', 'r' for 'right': ".format(candidate))
     if answer == "h": guess_num([candidate + 1, range[1]], counter + 1)   #restricts range to higher numbers
     elif answer == "l": guess_num([range[0], candidate - 1], counter + 1)   #restricts range to lower numbers
