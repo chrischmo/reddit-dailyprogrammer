0b3a18e9d8969ffbbcc8380ee06d05ce6830d699
Christian Schmitt <chrischmo@googlemail.com>
2012-02-10 16:52:05 +0100

    Simiplified expression for getting the medium value. Cosmetic changes

--- 120210_difficult.py
+++ 120210_difficult.py
@@ -1,11 +1,11 @@
 def guess_num(range, guesses = 1):
     """Tries to guess a number in a range given as an array by narrowing down the range recursively"""
-    candidate = range[0] + (range[1] - range[0]) / 2
+    candidate = (range[0] + range[1]) / 2  #medium value of current range
     answer = raw_input("Is it " + str(candidate) + "? Answer 'h' for 'higher', 'l' for 'lower', 'r' for 'right': ")
     if answer == "h": guess_num([candidate + 1, range[1]], guesses + 1)
     elif answer == "l": guess_num([range[0], candidate - 1], guesses + 1)
     elif answer == "r": print "HA! And it took me only " + str(guesses) + " guesses!"
-    else: guess_num(range, guesses)
+    else: guess_num(range, guesses)   #catches invalid answers
 
 num = raw_input("Please enter a whole number between 1 and 100 and I'll try to guess it: ")
 guess_num([1, 100])
