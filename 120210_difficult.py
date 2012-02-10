8ee26bc17f70e980fe6b076b35c6e9b14a8f95d0
Christian Schmitt <chrischmo@googlemail.com>
2012-02-10 15:31:11 +0100

    Some minor adjustments

--- 120210_difficult.py
+++ 120210_difficult.py
@@ -1,10 +1,10 @@
-def guess_num(range, guesses):
+def guess_num(range, guesses = 1):
     candidate = range[0] + (range[1] - range[0]) / 2
-    answer = raw_input("Is it " + str(candidate) + "? - 'h' for higher 'l', for lower, 'r' for right: ")
+    answer = raw_input("Is it " + str(candidate) + "? Answer 'h' for 'higher', 'l' for 'lower', 'r' for 'right': ")
     if answer == "h": guess_num([candidate + 1, range[1]], guesses + 1)
     elif answer == "l": guess_num([range[0], candidate - 1], guesses + 1)
-    elif answer == "r": print "YEAH! Needed " + str(guesses) + " guesses."
-    else: print "Invalid answer, let's try again: \n" + guess_num(range, guesses)
+    elif answer == "r": print "HA! And it took me only " + str(guesses) + " guesses!"
+    else: guess_num(range, guesses)
 
-num = raw_input("Please enter a whole number between 1 and 100: ")
-guess_num([1, 100], 1)
+num = raw_input("Please enter a whole number between 1 and 100 and I'll try to guess it: ")
+guess_num([1, 100])
